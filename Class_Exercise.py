b300ac01fbc150f971aa3a7c3f5e475c2118c678
ivela064 <113482288+ivela064@users.noreply.github.com>
2022-09-13 18:50:46 -0500

    Add files via upload

---
+++ Class_Exercise.py
@@ -0,0 +1,20 @@
+num_1 = int(input("Enter your first number: "))
+num_2 = int(input("Enter your second number: "))
+
+total_operation_1 = None
+total_operation_2 = None
+total_operation_3 = None
+total_operation_4 = None
+total_operation_5 = None
+
+total_operation_1 = num_1 - num_2
+total_operation_2 = num_1 + num_2
+total_operation_3 = num_1 * num_2
+total_operation_4 = num_1 ** num_2
+total_operation_5 = num_1 / num_2
+
+print("Minus operation result is:",total_operation_1)
+print("Sum operation result is:", total_operation_2)
+print("Multiplication operation result is:", total_operation_3)
+print("Power operation result is:", total_operation_4)
+print("Division operation result is:", total_operation_5)
