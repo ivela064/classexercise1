0dd589caf7428bed66079a88e7ea0e994ed2f904
Syed Mohid <113481552+P1ank@users.noreply.github.com>
2022-09-13 20:07:01 -0400

    Update Class_Exercise.py

--- Class_Exercise.py
+++ Class_Exercise.py
@@ -1,5 +1,6 @@
 num_1 = int(input("Enter your first integer number: "))
 num_2 = int(input("Enter your second integer number: "))
+num_3 = int(input("Enter your third integer number: "))
 
 total_operation_1 = None
 total_operation_2 = None
@@ -12,6 +13,18 @@ total_operation_2 = num_1 + num_2
 total_operation_3 = num_1 * num_2
 total_operation_4 = num_1 ** num_2
 total_operation_5 = num_1 / num_2
+
+print("Minus operation result is:",total_operation_1)
+print("Sum operation result is:", total_operation_2)
+print("Multiplication operation result is:", total_operation_3)
+print("Power operation result is:", total_operation_4)
+print("Division operation result is:", total_operation_5)
+
+total_operation_1 = total_operation_1+num_3
+total_operation_2 = total_operation_2-num_3
+total_operation_3 = total_operation_3*num_3
+total_operation_4 = total_operation_4**num_3
+total_operation_5 = total_operation_5/num_3
 
 print("Minus operation result is:",total_operation_1)
 print("Sum operation result is:", total_operation_2)
